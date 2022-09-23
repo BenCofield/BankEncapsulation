367d2d5976b8197c4b9b55d1d6628a04a99dfa13
Ben Cofield <benrcofield@gmail.com>
2022-09-23 14:48:36 -0400

    First commit

---
+++ BankEncapsulation/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public double GetBalance()
+        {
+            return Math.Round(balance, 2);
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BankEncapsulation;
+
+
+var account1 = new BankAccount();
+while (true)
+{
+    
+    Console.WriteLine("ATM\n" +
+                      "----------\n" +
+                      "1. Deposit\n" +
+                      "2. See Balance\n" +
+                      "3. Exit\n" +
+                      "----------\n" +
+                      "Choose from options: ");
+    var selection = Console.ReadLine();
+    switch (selection)
+    {
+        case "1":
+            Console.WriteLine("Enter deposit amount: ");
+            double depositAmount = double.Parse(Console.ReadLine());
+            account1.Deposit(depositAmount);
+            break;
+        case "2":
+            Console.WriteLine($"Your balance is ${account1.GetBalance()}.");
+            break;
+        case "3":
+            return;
+        default:
+            Console.WriteLine("Invalid input");
+            break;
+    }
+
+
+}
